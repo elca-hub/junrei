dfd81a07c6051ebdd7b88f1402703743fe640091
elca-hub <csubaru07@gmail.com>
2023-08-01 13:36:20 +0900

    テストケースの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: {maximum: 30}, uniqueness: true
+  validates :email, length: {maximum: 255}, presence: true, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,139 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { build(:user) }
+  subject { user } 
+
+  describe "バリデーション"  do
+    context "すべての属性が完璧で究極の場合" do
+      it "有効" do
+        is_expected.to be_valid
+      end
+    end
+
+    describe "name" do
+      NAME_LENGH = 30
+      context "値が#{NAME_LENGH}文字以内の場合" do
+        it "有効" do
+          user.name = "a" * NAME_LENGH
+
+          is_expected.to be_valid
+        end
+      end
+
+      context "値が#{NAME_LENGH + 1}文字以上の場合" do
+        it "無効" do
+          user.name = "a" * (NAME_LENGH + 1)
+
+          is_expected.to be_invalid
+        end
+      end
+
+      context "値が空の場合" do
+        it "無効" do
+          user.name = ""
+
+          is_expected.to be_invalid
+        end
+      end
+
+      context "値がnilの場合" do
+        it "無効" do
+          user.name = nil
+
+          is_expected.to be_invalid
+        end
+      end
+
+      context "値が重複している場合" do
+        DUPLICATE_NAME = "duplicate"
+        before { create(:user, name: DUPLICATE_NAME) }
+
+        it "無効" do
+          user.name = DUPLICATE_NAME
+
+          is_expected.to be_invalid
+        end
+      end
+    end
+
+    describe "email" do
+      context "値が255文字以内の場合" do
+        it "有効" do
+          user.email = "a" * 243 + "@example.com"
+
+          is_expected.to be_valid
+        end
+      end
+
+      context "値が256文字以上の場合" do
+        it "無効" do
+          user.email = "a" * 244 + "@example.com"
+
+          is_expected.to be_invalid
+        end
+      end
+
+      context "値が空の場合" do
+        it "無効" do
+          user.email = ""
+
+          is_expected.to be_invalid
+        end
+      end
+
+      context "値がnilの場合" do
+        it "無効" do
+          user.email = nil
+
+          is_expected.to be_invalid
+        end
+      end
+
+      context "値が重複している場合" do
+        DUPLICATE_EMAIL = "duplicate@test.com"
+        before { create(:user, email: DUPLICATE_EMAIL) }
+
+        it "無効" do
+          user.email = DUPLICATE_EMAIL
+          is_expected.to be_invalid
+        end
+      end
+    end
+
+    describe "password" do
+      MIN_PASSWORD_LENGTH = 6
+      context "値が#{MIN_PASSWORD_LENGTH}文字以上の場合" do
+        it "有効" do
+          user.password = "a" * MIN_PASSWORD_LENGTH
+  
+          is_expected.to be_valid
+        end
+      end
+
+      context "値が#{MIN_PASSWORD_LENGTH - 1}文字以下の場合" do
+        it "無効" do
+          user.password = "a" * (MIN_PASSWORD_LENGTH - 1)
+
+          is_expected.to be_invalid
+        end
+      end
+
+      context "値が空の場合" do
+        it "無効" do
+          user.password = ""
+
+          is_expected.to be_invalid
+        end
+      end
+
+      context "値がnilの場合" do
+        it "無効" do
+          user.password = nil
+
+          is_expected.to be_invalid
+        end
+      end
+    end
+  end
+end
