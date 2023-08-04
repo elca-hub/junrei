efc5391ddd44f10d48e134c37ef79188610d4713
elca-hub <csubaru07@gmail.com>
2023-08-04 16:18:43 +0900

    seedの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ActiveRecord::Base.transaction do
+  Dir.glob(File.join(Rails.root, 'db', 'seeds', '*.rb')).sort.each do |file|
+    puts "-- seeding data from #{file}"
+    load file
+  end
+end
