efc080c1037a7c4c49f865248c767aa70da16f4c
elca-hub <csubaru07@gmail.com>
2023-08-04 23:59:29 +0900

    ヘルパーメソッドの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,22 @@
 module ApplicationHelper
   def full_title(page_title = "")
     base = "junrei" # アプリ名
-    return page_title.present? ? "#{page_title} | #{base}" : base
+    page_title.present? ? "#{page_title} | #{base}" : base
+  end
+
+  def choice_dark_or_light
+    start_dark_mode_time = 19
+    end_dark_mode_time = 5
+
+    now = Time.now
+    if now.hour >= start_dark_mode_time || now.hour <= end_dark_mode_time
+      "dark"
+    else
+      "light"
+    end
+  end
+
+  def view_time(time)
+    time.strftime('%Y/%m/%d/ %H:%M')
   end
 end
