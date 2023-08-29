15dcdb0b3c9d3f97285c0fb59ba993456bb42b2d
elca-hub <csubaru07@gmail.com>
2023-08-29 17:03:26 +0900

    ユーザを削除する際のテストケースの作成

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -25,4 +25,38 @@ RSpec.describe "Users", type: :request do
       end
     end
   end
+
+  describe "DELETE /users" do
+    let!(:deleted_user) { create(:user) }
+
+    context "ログインのとき" do
+      before do
+        sign_in deleted_user
+
+        delete user_registration_path
+      end
+
+      it "rootへリダイレクト" do
+        expect(response).to redirect_to root_path
+      end
+
+      it "ユーザーが削除される" do
+        expect(User.find_by(id: deleted_user.id)).to be_nil
+      end
+    end
+
+    context "未ログインのとき" do
+      before do
+        delete user_registration_path
+      end
+
+      it "/users/sign_inへリダイレクト" do
+        expect(response).to redirect_to user_session_path
+      end
+
+      it "ユーザーが削除されない" do
+        expect(User.find_by(id: deleted_user.id)).not_to be_nil
+      end
+    end
+  end
 end
