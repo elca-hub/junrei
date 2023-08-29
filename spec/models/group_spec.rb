98227cfac50acea54904dfd5398bddde018401ce
elca-hub <csubaru07@gmail.com>
2023-08-29 23:24:48 +0900

    グループ名の重複に関するテストケースの修正

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Group, type: :model do
   subject { group }
 
-  let(:group) { build(:group) }
+  let(:user) { build(:user) }
+  let(:group) { build(:group, user:) }
 
   describe 'バリデーション' do
     context '全ての要素が天才的なグループ様の場合' do
@@ -38,14 +39,30 @@ RSpec.describe Group, type: :model do
       end
     end
 
-    context '値が重複している場合' do
+    describe '値が重複している場合' do
       duplicate_group_name = 'duplicate'
-      before { create(:group, name: duplicate_group_name) }
 
-      it '有効' do
-        group.name = duplicate_group_name
+      context '自身のグループの場合' do
+        before { create(:group, name: duplicate_group_name, user:) }
 
-        expect(subject).to be_valid
+        it '無効' do
+          group.name = duplicate_group_name
+
+          expect(subject).to be_invalid
+        end
+      end
+
+      context '他人のグループの場合' do
+        before do
+          other_user = create(:user)
+          create(:group, name: duplicate_group_name, user: other_user)
+        end
+
+        it '有効' do
+          group.name = duplicate_group_name
+
+          expect(subject).to be_valid
+        end
       end
     end
   end
