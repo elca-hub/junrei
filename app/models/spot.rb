87ecc67dd619acdc7befc9e5aa6fe6538804f078
elca-hub <csubaru07@gmail.com>
2023-08-29 16:02:45 +0900

    spotのテストケースの作成

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -10,4 +10,13 @@ class Spot < ApplicationRecord
     validates :memo, length: {maximum: 200}
     validates :is_achieved, inclusion: {in: [true, false]}
     validates :sort_index, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
+    # 同じグループで同じsort_indexが存在しないかチェックする
+    validate :check_duplicate_sort_index
+
+    def check_duplicate_sort_index
+        if self.group.spots.where(sort_index: self.sort_index).where.not(id: self.id).count > 0
+            errors.add(:sort_index, "is duplicated")
+        end
+    end
 end
--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -1,5 +1,157 @@
 require 'rails_helper'
 
 RSpec.describe Spot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:spot) { build(:spot) }
+  subject { spot }
+
+  describe "バリデーション" do
+    context "全ての要素が天才的なスポット様の場合" do
+      it "有効" do
+        is_expected.to be_valid
+      end
+    end
+
+    describe "name" do
+      context "値が100文字以内の場合" do
+        it "有効" do
+          spot.name = "a" * 100
+
+          is_expected.to be_valid
+        end
+      end
+
+      context "値が101文字以上の場合" do
+        it "無効" do
+          spot.name = "a" * 101
+
+          is_expected.to be_invalid
+        end
+      end
+
+      context "値が空の場合" do
+        it "無効" do
+          spot.name = ""
+
+          is_expected.to be_invalid
+        end
+      end
+
+      context "値が重複している場合" do
+        DUPLICATE_SPOT_NAME = "duplicate spot"
+        before { create(:spot, name: DUPLICATE_SPOT_NAME) }
+  
+        it "有効" do
+          spot.name = DUPLICATE_SPOT_NAME
+  
+          is_expected.to be_valid
+        end
+      end
+    end
+
+    describe "memo" do
+      context "値が200文字以内の場合" do
+        it "有効" do
+          spot.memo = "a" * 200
+
+          is_expected.to be_valid
+        end
+      end
+
+      context "値が201文字以上の場合" do
+        it "無効" do
+          spot.memo = "a" * 201
+
+          is_expected.to be_invalid
+        end
+      end
+
+      context "値が空の場合" do
+        it "有効" do
+          spot.memo = ""
+
+          is_expected.to be_valid
+        end
+      end
+
+      context "値が重複している場合" do
+        DUPLICATE_SPOT_MEMO = "duplicate spot memo"
+        before { create(:spot, memo: DUPLICATE_SPOT_MEMO) }
+  
+        it "有効" do
+          spot.memo = DUPLICATE_SPOT_MEMO
+  
+          is_expected.to be_valid
+        end
+      end
+    end
+
+    describe "is_achieved" do
+      context "値がtrueの場合" do
+        it "有効" do
+          spot.is_achieved = true
+  
+          is_expected.to be_valid
+        end
+      end
+  
+      context "値がfalseの場合" do
+        it "有効" do
+          spot.is_achieved = false
+  
+          is_expected.to be_valid
+        end
+      end
+  
+      context "値がnilの場合" do
+        it "無効" do
+          spot.is_achieved = nil
+  
+          is_expected.to be_invalid
+        end
+      end
+    end
+
+    describe "sort_index" do
+      context "値が0以上の場合" do
+        it "有効" do
+          spot.sort_index = 0
+
+          is_expected.to be_valid
+        end
+      end
+
+      context "値が0未満の場合" do
+        it "無効" do
+          spot.sort_index = -1
+
+          is_expected.to be_invalid
+        end
+      end
+
+      describe "重複するsort_indexが存在する" do
+        DUPLICATE_SORT_INDEX = 0
+        before do
+          @group = create(:group)
+          duplicate_spot = create(:spot, sort_index: DUPLICATE_SORT_INDEX, group: @group)
+        end
+
+        context "異なるグループの場合" do
+          it "有効" do
+            spot.sort_index = DUPLICATE_SORT_INDEX
+
+            is_expected.to be_valid
+          end
+        end
+
+        context "同じグループの場合" do
+          it "無効" do
+            spot.sort_index = DUPLICATE_SORT_INDEX
+            spot.group = @group
+
+            is_expected.to be_invalid
+          end
+        end
+      end
+    end
+  end
 end
