70408aa759c856f8ec59837e3d1e163eb59ebab2
elca-hub <csubaru07@gmail.com>
2023-08-29 15:40:58 +0900

    groupのバリデーションを追加

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :group do
-    
+    association :user
+    sequence(:name) { |n| "testGroup#{n}" }
+    comment { "testComment" }
+    is_achieved { false }
   end
 end
