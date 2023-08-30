aa767a589c0cac7efad86d1f41fffa96e99fc504
elca-hub <csubaru07@gmail.com>
2023-08-30 10:59:43 +0900

    全スポットを削除するリクエストのテストケースを追加

--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -62,4 +62,47 @@ RSpec.describe 'Groups', type: :request do
       end
     end
   end
+
+  describe 'DELETE /groups/:group_id/delete_all_spots' do
+
+    context '自身のグループのとき' do
+      before do
+        sign_in user
+
+        @group = create(:group, user:)
+        create_list(:spot, 3, group: @group)
+
+        delete group_destroy_all_spots_path(@group)
+      end
+
+      it 'スポットが全て削除される' do
+        expect(@group.spots.count).to eq 0
+      end
+
+      it '/groups/:group_idへリダイレクト' do
+        expect(response).to redirect_to group_path(@group)
+      end
+    end
+
+    context '自身のグループでないとき' do
+      before do
+        sign_in user
+
+        @other_user = create(:user)
+        @other_group = create(:group, user: @other_user)
+
+        create_list(:spot, 3, group: @other_group)
+
+        delete group_destroy_all_spots_path(@other_group)
+      end
+
+      it 'スポットが削除されない' do
+        expect(@other_group.spots.count).to eq 3
+      end
+
+      it '/groupsへリダイレクト' do
+        expect(response).to redirect_to groups_path
+      end
+    end
+  end
 end
